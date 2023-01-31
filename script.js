6e19631d2699fb0f31a726b9a77de8825349df97
kenzietzu <kenzietzu@gmail.com>
2023-01-31 19:09:22 +0800

    add idTag

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-const monthTag = document.querySelector(".this-month")
-const weekContainer = document.querySelector(".week-container")
-const prev = document.querySelector(".prev")
-const next = document.querySelector(".next")
+const monthTag = document.querySelector(".this-month");
+const weekContainer = document.querySelector(".week-container");
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+const todayBtn = document.querySelector(".today-btn");
 
 let today = new Date();
-let currMonth = today.getMonth();
-let currYear = today.getFullYear();
+let realToday = new Date();
+let currMonth;
+let currYear;
 let thisMon = new Date();
 let lastMon = new Date();
 let nextMon = new Date();
@@ -28,6 +30,7 @@ const months = [
     "December",
 ];
 
+let id = [];
 let weekArray = [];
 const dayArray = ['MON', 'TUE', 'WEN', 'THU', 'FRI', 'SAT', 'SUN'];
 
@@ -40,23 +43,15 @@ function buildCalendar() {
         const date = document.createElement('div');
         date.textContent = weekArray[i];
         date.classList.add('day');
+        const event = document.createElement('div');
+        event.textContent = '';
+        event.classList.add('event');
+        event.setAttribute('id', id[i])
         const column = document.createElement('div');
         column.classList.add('column');
-        column.append(day, date);
+        column.append(day, date, event);
         weekContainer.append(column);
     }
-
-    // weekArray.forEach((dateTag) => {
-    //     const day = document.createElement('h2');
-    //     day.classList.add('h2');
-    //     const date = document.createElement('div');
-    //     date.textContent = `${dateTag}`;
-    //     date.classList.add('day');
-    //     const column = document.createElement('div');
-    //     column.classList.add('column');
-    //     column.append(day, date);
-    //     weekContainer.append(column);
-    // })
 }
 
 function getMonday() {
@@ -68,24 +63,61 @@ function getMonday() {
 }
 
 function initCalendar() {
+    currMonth = thisMon.getMonth();
+    currYear = thisMon.getFullYear();
     monthTag.innerHTML = `${months[currMonth]} ${currYear}`;
     weekContainer.textContent = '';
     getMonday();
     console.log('this Monday: ', thisMon);
     weekArray[0] = thisMon.getDate();
+    currMonth = thisMon.getMonth();
+    currYear = thisMon.getFullYear();
+    id[0] = `${currYear}${months[currMonth]}${weekArray[0]}`;
     thisMon.setDate(thisMon.getDate() + 1); weekArray[1] = thisMon.getDate();
+    currMonth = thisMon.getMonth();
+    currYear = thisMon.getFullYear();
+    id[1] = `${currYear}${months[currMonth]}${weekArray[1]}`;
     thisMon.setDate(thisMon.getDate() + 1); weekArray[2] = thisMon.getDate();
+    currMonth = thisMon.getMonth();
+    currYear = thisMon.getFullYear();
+    id[2] = `${currYear}${months[currMonth]}${weekArray[2]}`;
     thisMon.setDate(thisMon.getDate() + 1); weekArray[3] = thisMon.getDate();
+    currMonth = thisMon.getMonth();
+    currYear = thisMon.getFullYear();
+    id[3] = `${currYear}${months[currMonth]}${weekArray[3]}`;
     thisMon.setDate(thisMon.getDate() + 1); weekArray[4] = thisMon.getDate();
+    currMonth = thisMon.getMonth();
+    currYear = thisMon.getFullYear();
+    id[4] = `${currYear}${months[currMonth]}${weekArray[4]}`;
     thisMon.setDate(thisMon.getDate() + 1); weekArray[5] = thisMon.getDate();
+    currMonth = thisMon.getMonth();
+    currYear = thisMon.getFullYear();
+    id[5] = `${currYear}${months[currMonth]}${weekArray[5]}`;
     thisMon.setDate(thisMon.getDate() + 1); weekArray[6] = thisMon.getDate();
+    currMonth = thisMon.getMonth();
+    currYear = thisMon.getFullYear();
+    id[6] = `${currYear}${months[currMonth]}${weekArray[6]}`;
     console.log('this Sunday: ', thisMon);
     console.log(weekArray);
+    console.log(id);
+    
 
     buildCalendar();
-
+    getToday();
 }
+
 initCalendar();
+
+function getToday() {
+    const todayTag = document.querySelectorAll("div.day")
+    // console.log(realToday.getDate());
+    // console.log(todayTag[1].textContent);
+    for (let i = 0; i < 7; i++) {
+        if (realToday.getDate().toString() === todayTag[i].textContent) {
+            todayTag[i].classList.add('today');
+        }
+    }
+}
 
 function getNextMonday() {
     const next = nowMon.getDate() - nowMon.getDay() + 8;
@@ -111,15 +143,37 @@ function getNextWeek() {
     monthTag.innerHTML = `${months[currMonth]} ${currYear}`;
     console.log('this Monday: ', nextMon);
     weekArray[0] = nextMon.getDate();
+    currMonth = nextMon.getMonth();
+    currYear = nextMon.getFullYear();
+    id[0] = `${currYear}${months[currMonth]}${weekArray[0]}`;
     nextMon.setDate(nextMon.getDate() + 1); weekArray[1] = nextMon.getDate();
+    currMonth = nextMon.getMonth();
+    currYear = nextMon.getFullYear();
+    id[1] = `${currYear}${months[currMonth]}${weekArray[1]}`;
     nextMon.setDate(nextMon.getDate() + 1); weekArray[2] = nextMon.getDate();
+    currMonth = nextMon.getMonth();
+    currYear = nextMon.getFullYear();
+    id[2] = `${currYear}${months[currMonth]}${weekArray[2]}`;
     nextMon.setDate(nextMon.getDate() + 1); weekArray[3] = nextMon.getDate();
+    currMonth = nextMon.getMonth();
+    currYear = nextMon.getFullYear();
+    id[3] = `${currYear}${months[currMonth]}${weekArray[3]}`;
     nextMon.setDate(nextMon.getDate() + 1); weekArray[4] = nextMon.getDate();
+    currMonth = nextMon.getMonth();
+    currYear = nextMon.getFullYear();
+    id[4] = `${currYear}${months[currMonth]}${weekArray[4]}`;
     nextMon.setDate(nextMon.getDate() + 1); weekArray[5] = nextMon.getDate();
+    currMonth = nextMon.getMonth();
+    currYear = nextMon.getFullYear();
+    id[5] = `${currYear}${months[currMonth]}${weekArray[5]}`;
     nextMon.setDate(nextMon.getDate() + 1); weekArray[6] = nextMon.getDate();
+    currMonth = nextMon.getMonth();
+    currYear = nextMon.getFullYear();
+    id[6] = `${currYear}${months[currMonth]}${weekArray[6]}`;
     console.log(weekArray);
  
     buildCalendar();
+    getToday();
 }
 
 function getLastWeek() {
@@ -130,17 +184,66 @@ function getLastWeek() {
     monthTag.innerHTML = `${months[currMonth]} ${currYear}`;
     console.log('last Monday: ', lastMon);
     weekArray[0] = lastMon.getDate();
+    currMonth = lastMon.getMonth();
+    currYear = lastMon.getFullYear();
+    id[0] = `${currYear}${months[currMonth]}${weekArray[0]}`;
     lastMon.setDate(lastMon.getDate() + 1); weekArray[1] = lastMon.getDate();
+    currMonth = lastMon.getMonth();
+    currYear = lastMon.getFullYear();
+    id[1] = `${currYear}${months[currMonth]}${weekArray[1]}`;
     lastMon.setDate(lastMon.getDate() + 1); weekArray[2] = lastMon.getDate();
+    currMonth = lastMon.getMonth();
+    currYear = lastMon.getFullYear();
+    id[2] = `${currYear}${months[currMonth]}${weekArray[2]}`;
     lastMon.setDate(lastMon.getDate() + 1); weekArray[3] = lastMon.getDate();
+    currMonth = lastMon.getMonth();
+    currYear = lastMon.getFullYear();
+    id[3] = `${currYear}${months[currMonth]}${weekArray[3]}`;
     lastMon.setDate(lastMon.getDate() + 1); weekArray[4] = lastMon.getDate();
+    currMonth = lastMon.getMonth();
+    currYear = lastMon.getFullYear();
+    id[4] = `${currYear}${months[currMonth]}${weekArray[4]}`;
     lastMon.setDate(lastMon.getDate() + 1); weekArray[5] = lastMon.getDate();
+    currMonth = lastMon.getMonth();
+    currYear = lastMon.getFullYear();
+    id[5] = `${currYear}${months[currMonth]}${weekArray[5]}`;
     lastMon.setDate(lastMon.getDate() + 1); weekArray[6] = lastMon.getDate();
+    currMonth = lastMon.getMonth();
+    currYear = lastMon.getFullYear();
+    id[6] = `${currYear}${months[currMonth]}${weekArray[6]}`;
     console.log(weekArray);
  
 
     buildCalendar();
+    getToday();
 }
 
 prev.addEventListener("click", getLastWeek);
 next.addEventListener("click", getNextWeek);
+
+todayBtn.addEventListener("click", () => {
+    today = new Date();
+    month = today.getMonth();
+    year = today.getFullYear();
+    initCalendar();
+});
+
+
+
+
+
+
+
+//function to save events in local storage
+function saveEvents() {
+    localStorage.setItem("events", JSON.stringify(eventsArr));
+}
+
+//function to get events from local storage
+function getEvents() {
+    //check if events are already saved in local storage then return event else nothing
+    if (localStorage.getItem("events") === null) {
+        return;
+    }
+    eventsArr.push(...JSON.parse(localStorage.getItem("events")));
+}
